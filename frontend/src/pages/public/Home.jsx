699afd615fc510b910dfb5a71757088dbdea3032
gk3204 <gk200432@gmail.com>
2026-04-12 22:12:33 +0530

    New changes

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -133,7 +133,7 @@ React.useEffect(() => {
           Room Highlights
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {[
             {
               name: "Double Deluxe",
